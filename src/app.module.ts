535103ac6c5ea834582c19843e0295fa4d8f83f9
José Cortés <cote99salamanca@gmail.com>
2020-12-23 00:50:14 -0300

    nest js api example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MessagesController } from './messages/messages.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MessagesService } from './messages/messages.service';
+import { Message } from './messages/Entities/mesagge.entity';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  //modulos o referencias
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'sendmeapp_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Message]),
+  ],
+  //todos los controladores del modulo, un modulo agrupa varios
+  controllers: [AppController, MessagesController],
+  //los servicios donde se encuentras los metodos de acceso a los datos
+  providers: [AppService, MessagesService],
 })
 export class AppModule {}
